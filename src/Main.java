00baa5ee59b2946eda2c9271e3d2a32a92a6f183
mateor1309-admin <mateo.torres@epn.edu.ec>
2024-05-30 08:39:21 -0500

    prueba 4.1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import procesadores.procesadores;
+import procesadores.procesadores_AMD;
+import procesadores.Procesadores_moviles;
+
 public class Main {
     public static void main(String[] args) {
 
-        procesadores p1 = new procesadores();
-
+        //integrante 1
+        procesadores_AMD p1 = new procesadores_AMD();
+        procesadores_AMD p2 = new procesadores_AMD(10, 5.0, 7.0,"Ryzen 5", "AM4", true);
     }
 }
