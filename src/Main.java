541948f9edecc28544ff588624b32a132eccfdd3
mateor1309-admin <mateo.torres@epn.edu.ec>
2024-05-30 08:47:18 -0500

    prueba 4.1

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,8 @@ public class Main {
         //integrante 2
         procesadores_intel p3 = new procesadores_intel();
         procesadores_intel p4 = new procesadores_intel(6,95.0,3.5,"Core i5","LGA1156",true);
+        //integrante 1
+        Procesadores_moviles p5 = new Procesadores_moviles();
+        Procesadores_moviles p6 = new Procesadores_moviles(4,3.5,7.9,"Snapdragon 8G",true);
     }
 }
