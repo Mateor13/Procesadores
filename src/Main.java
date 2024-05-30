57fc8b8fcb2dfd98ae38482775491b70838e84f7
anthony55234 <anthony.astudillo@epn.edu.ec>
2024-05-30 08:45:02 -0500

    CAMBIO CUATRO

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import procesadores.procesadores;
 import procesadores.procesadores_AMD;
 import procesadores.Procesadores_moviles;
+import procesadores.procesadores_intel;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,5 +9,8 @@ public class Main {
         //integrante 1
         procesadores_AMD p1 = new procesadores_AMD();
         procesadores_AMD p2 = new procesadores_AMD(10, 5.0, 7.0,"Ryzen 5", "AM4", true);
+        //integrante 2
+        procesadores_intel p3 = new procesadores_intel();
+        procesadores_intel p4 = new procesadores_intel(6,95.0,3.5,"Core i5","LGA1156",true);
     }
 }
